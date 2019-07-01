6641eca89f5d281ea1b6ac1e098408fd04874afb
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2019-07-01 11:45:39 +0200

    - implemented fix for changed json structure in galaxy api

--- lib/librarian/ansible/source/galaxy.rb
+++ lib/librarian/ansible/source/galaxy.rb
@@ -58,13 +58,13 @@ module Librarian
                 break if json['results'].nil?
 
                 package = json['results'].find do |r|
-                  r['namespace'] == username &&
-                    r['name'] == name
+                  r['summary_fields']['namespace']['name'] == username &&
+                      r['name'] == name
                 end
                 return package if package
 
                 break unless json['next']
-                url = "#{@@galaxy_api}/#{json['next']}"
+                url = "#{@@galaxy_api}#{json['next']}"
               end
             end
           end
